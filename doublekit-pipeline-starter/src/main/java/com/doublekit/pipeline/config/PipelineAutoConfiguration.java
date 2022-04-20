512610cfd5587f88f8027717e7f2102c34b74117
zhangzhihua <zhangzhihua@darthcloud.com>
2022-04-20 17:15:01 +0800

    refactor eam/iam/gateway

--- doublekit-pipeline-starter/src/main/java/com/doublekit/pipeline/config/PipelineAutoConfiguration.java
+++ doublekit-pipeline-starter/src/main/java/com/doublekit/pipeline/config/PipelineAutoConfiguration.java
@@ -1,7 +1,7 @@
 package com.doublekit.pipeline.config;
 
 
-import com.doublekit.apibox.client.annotation.EnableApiboxClient;
+import com.doublekit.apibox.client.EnableApiboxClient;
 import com.doublekit.beans.starter.annotation.EnableBeans;
 import com.doublekit.dal.starter.annotation.EnableDal;
 
@@ -9,7 +9,6 @@ import com.doublekit.dcs.starter.annotation.EnableDcs;
 import com.doublekit.dfs.starter.annotation.EnableDfs;
 import com.doublekit.join.starter.annotation.EnableJoin;
 import com.doublekit.dss.starter.annotation.EnableDss;
-import com.doublekit.rpc.starter.annotation.EnableRpc;
 import com.doublekit.web.starter.annotation.EnableWeb;
 import com.doublekit.pipeline.annotation.EnablePipelineServer;
 import org.springframework.context.annotation.Configuration;
