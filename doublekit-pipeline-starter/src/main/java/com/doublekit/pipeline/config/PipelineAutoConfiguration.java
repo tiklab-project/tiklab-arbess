2e6b252175e1c61eb604d25fc70910056578ba43
zhangzhihua <zhangzhihua@darthcloud.com>
2022-04-24 16:47:11 +0800

    update dxs/eam/rpc config

--- doublekit-pipeline-starter/src/main/java/com/doublekit/pipeline/config/PipelineAutoConfiguration.java
+++ doublekit-pipeline-starter/src/main/java/com/doublekit/pipeline/config/PipelineAutoConfiguration.java
@@ -3,10 +3,10 @@ package com.doublekit.pipeline.config;
 import com.doublekit.beans.starter.annotation.EnableBeans;
 import com.doublekit.dal.starter.annotation.EnableDal;
 
-import com.doublekit.dcs.starter.annotation.EnableDcs;
-import com.doublekit.dfs.starter.annotation.EnableDfs;
+import com.doublekit.dcs.starter.EnableDcs;
+import com.doublekit.dfs.starter.EnableDfs;
 import com.doublekit.join.starter.annotation.EnableJoin;
-import com.doublekit.dss.starter.annotation.EnableDss;
+import com.doublekit.dss.starter.EnableDss;
 import com.doublekit.web.starter.annotation.EnableWeb;
 import com.doublekit.pipeline.annotation.EnablePipelineServer;
 import org.springframework.context.annotation.Configuration;
