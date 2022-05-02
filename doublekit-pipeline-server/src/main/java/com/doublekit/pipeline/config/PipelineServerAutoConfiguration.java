07b824d60d5cde4d262fbc8967b2ac6abe059f1e
zhangzhihua <zhangzhihua@darthcloud.com>
2022-05-02 22:02:43 +0800

    reafctor datafly->dsm

--- doublekit-pipeline-server/src/main/java/com/doublekit/pipeline/config/PipelineServerAutoConfiguration.java
+++ doublekit-pipeline-server/src/main/java/com/doublekit/pipeline/config/PipelineServerAutoConfiguration.java
@@ -1,14 +1,14 @@
 package com.doublekit.pipeline.config;
 
 
-import com.doublekit.datafly.annotation.DataFly;
+import com.doublekit.dsm.annotation.Dsm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@DataFly(modules = "pipeline")
+@Dsm(modules = "pipeline")
 @ComponentScan({"com.doublekit.pipeline"})
 public class PipelineServerAutoConfiguration {
 
