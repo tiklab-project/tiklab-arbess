f0ed76074b49d0d0a1de3401b7906419b19e904a
zhangcheng <zhq11191750>
2022-08-29 18:35:42 +0800

    gitUpdate

--- tiklab-matflow-server/src/main/java/com/tiklab/matflow/execute/service/codeGit/CodeCheckServiceImpl.java
+++ tiklab-matflow-server/src/main/java/com/tiklab/matflow/execute/service/codeGit/CodeCheckServiceImpl.java
@@ -4,24 +4,10 @@ import com.tiklab.matflow.definition.service.MatFlowCommonService;
 import com.tiklab.matflow.execute.model.CodeGit.CodeCheckAuth;
 import com.tiklab.matflow.setting.proof.model.Proof;
 import com.tiklab.matflow.setting.proof.service.ProofService;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
 import com.tiklab.rpc.annotation.Exporter;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.LsRemoteCommand;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.transport.*;
-import org.eclipse.jgit.util.FS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.tmatesoft.svn.core.SVNException;
-import org.tmatesoft.svn.core.SVNURL;
-import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
-import org.tmatesoft.svn.core.internal.wc.DefaultSVNOptions;
-import org.tmatesoft.svn.core.io.SVNRepository;
-import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
-import org.tmatesoft.svn.core.wc.SVNWCUtil;
+
 
 import java.io.File;
 import java.io.IOException;
@@ -54,49 +40,49 @@ public class CodeCheckServiceImpl implements CodeCheckService {
      * @return 状态
      */
     public boolean gitCheck(String gitUrl, Proof proof) {
-        File file = null;
-        String path = "";
-        try {
-            file = File.createTempFile("matFlow", ".txt");
-            path = file.getAbsolutePath();
-            matFlowCommonService.writePrivateKeyPath(proof.getProofPassword(),path);
-        } catch (IOException e) {
-            if (file != null){
-                file.deleteOnExit();
-            }
-            return false;
-        }
-        String finalPath = path;
-        SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
-            @Override
-            protected void configure(OpenSshConfig.Host host, Session session ) {
-                //取消host文件验证
-                session.setConfig("StrictHostKeyChecking","no");
-            }
-            @Override
-            protected JSch createDefaultJSch(FS fs) throws JSchException {
-                JSch defaultJSch = super.createDefaultJSch(fs);
-                defaultJSch.addIdentity(finalPath);
-                return defaultJSch;
-            }
-        };
-        LsRemoteCommand lsRemoteCommand = Git.lsRemoteRepository()
-                .setRemote(gitUrl);
-        try {
-            if (proof.getProofType().equals("SSH")){
-                lsRemoteCommand .setTransportConfigCallback(transport -> {
-                            SshTransport sshTransport = (SshTransport) transport;
-                            sshTransport.setSshSessionFactory(sshSessionFactory);
-                        });
-            }else {
-                UsernamePasswordCredentialsProvider provider = new UsernamePasswordCredentialsProvider(proof.getProofUsername(), proof.getProofPassword());
-                lsRemoteCommand.setCredentialsProvider(provider);
-            }
-            lsRemoteCommand.callAsMap();
-        } catch (GitAPIException e) {
-            return false;
-        }
-        file.deleteOnExit();
+        //File file = null;
+        //String path = "";
+        //try {
+        //    file = File.createTempFile("matFlow", ".txt");
+        //    path = file.getAbsolutePath();
+        //    matFlowCommonService.writePrivateKeyPath(proof.getProofPassword(),path);
+        //} catch (IOException e) {
+        //    if (file != null){
+        //        file.deleteOnExit();
+        //    }
+        //    return false;
+        //}
+        //String finalPath = path;
+        //SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
+        //    @Override
+        //    protected void configure(OpenSshConfig.Host host, Session session ) {
+        //        //取消host文件验证
+        //        session.setConfig("StrictHostKeyChecking","no");
+        //    }
+        //    @Override
+        //    protected JSch createDefaultJSch(FS fs) throws JSchException {
+        //        JSch defaultJSch = super.createDefaultJSch(fs);
+        //        defaultJSch.addIdentity(finalPath);
+        //        return defaultJSch;
+        //    }
+        //};
+        //LsRemoteCommand lsRemoteCommand = Git.lsRemoteRepository()
+        //        .setRemote(gitUrl);
+        //try {
+        //    if (proof.getProofType().equals("SSH")){
+        //        lsRemoteCommand .setTransportConfigCallback(transport -> {
+        //                    SshTransport sshTransport = (SshTransport) transport;
+        //                    sshTransport.setSshSessionFactory(sshSessionFactory);
+        //                });
+        //    }else {
+        //        UsernamePasswordCredentialsProvider provider = new UsernamePasswordCredentialsProvider(proof.getProofUsername(), proof.getProofPassword());
+        //        lsRemoteCommand.setCredentialsProvider(provider);
+        //    }
+        //    lsRemoteCommand.callAsMap();
+        //} catch (GitAPIException e) {
+        //    return false;
+        //}
+        //file.deleteOnExit();
         return true;
     }
 
@@ -107,22 +93,22 @@ public class CodeCheckServiceImpl implements CodeCheckService {
      * @return 状态
      */
     public  boolean svnCheck(String svnUrl,Proof proof) {
-        BasicAuthenticationManager auth;
-        if (proof.getProofType().equals("SSH")){
-             auth = BasicAuthenticationManager
-                    .newInstance(proof.getProofUsername(), new File(proof.getProofPassword()),null,22);
-        }else {
-            auth = BasicAuthenticationManager.newInstance(proof.getProofUsername(), proof.getProofPassword().toCharArray());
-        }
-        DefaultSVNOptions options = SVNWCUtil.createDefaultOptions(true);
-        options.setDiffCommand("-x -w");
-        try {
-            SVNRepository repos = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(svnUrl));
-            repos.setAuthenticationManager(auth);
-            repos.testConnection();
-        } catch (SVNException e) {
-            return false;
-        }
+        //BasicAuthenticationManager auth;
+        //if (proof.getProofType().equals("SSH")){
+        //     auth = BasicAuthenticationManager
+        //            .newInstance(proof.getProofUsername(), new File(proof.getProofPassword()),null,22);
+        //}else {
+        //    auth = BasicAuthenticationManager.newInstance(proof.getProofUsername(), proof.getProofPassword().toCharArray());
+        //}
+        //DefaultSVNOptions options = SVNWCUtil.createDefaultOptions(true);
+        //options.setDiffCommand("-x -w");
+        //try {
+        //    SVNRepository repos = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(svnUrl));
+        //    repos.setAuthenticationManager(auth);
+        //    repos.testConnection();
+        //} catch (SVNException e) {
+        //    return false;
+        //}
         return true;
     }
 
@@ -133,38 +119,38 @@ public class CodeCheckServiceImpl implements CodeCheckService {
      */
     public Boolean deployCheck(String svnUrl,Proof proof , int port){
 
-        if (proof ==null){
-            return null;
-        }
-        JSch jsch = new JSch();
-        //采用指定的端口连接服务器
-        Session session;
-        try {
-            session = jsch.getSession(proof.getProofUsername(), svnUrl ,port);
-        } catch (JSchException e) {
-            return false;
-        }
-
-        //如果服务器连接不上，则抛出异常
-        if (session == null) {
-            return false;
-        }
-        //设置第一次登陆的时候提示，可选值：(ask | yes | no)
-        session.setConfig("StrictHostKeyChecking", "no");
-        try {
-            if (proof.getProofType().equals("password")){
-                //设置登陆主机的密码
-                session.setPassword(proof.getProofPassword());
-            }else {
-                //添加私钥
-                jsch.addIdentity(proof.getProofPassword());
-            }
-            //设置登陆超时时间 10s
-            session.connect(10000);
-        } catch (JSchException e) {
-            return  false;
-        }
-        session.disconnect();
+        //if (proof ==null){
+        //    return null;
+        //}
+        //JSch jsch = new JSch();
+        ////采用指定的端口连接服务器
+        //Session session;
+        //try {
+        //    session = jsch.getSession(proof.getProofUsername(), svnUrl ,port);
+        //} catch (JSchException e) {
+        //    return false;
+        //}
+        //
+        ////如果服务器连接不上，则抛出异常
+        //if (session == null) {
+        //    return false;
+        //}
+        ////设置第一次登陆的时候提示，可选值：(ask | yes | no)
+        //session.setConfig("StrictHostKeyChecking", "no");
+        //try {
+        //    if (proof.getProofType().equals("password")){
+        //        //设置登陆主机的密码
+        //        session.setPassword(proof.getProofPassword());
+        //    }else {
+        //        //添加私钥
+        //        jsch.addIdentity(proof.getProofPassword());
+        //    }
+        //    //设置登陆超时时间 10s
+        //    session.connect(10000);
+        //} catch (JSchException e) {
+        //    return  false;
+        //}
+        //session.disconnect();
         return true;
     }
 
--- tiklab-matflow-server/src/main/java/com/tiklab/matflow/instance/service/execAchieveImpl/CodeAchieveServiceImpl.java
+++ tiklab-matflow-server/src/main/java/com/tiklab/matflow/instance/service/execAchieveImpl/CodeAchieveServiceImpl.java
@@ -1,8 +1,6 @@
 package com.tiklab.matflow.instance.service.execAchieveImpl;
 
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
+
 import com.tiklab.matflow.definition.model.MatFlowConfigure;
 import com.tiklab.matflow.definition.service.MatFlowCommonService;
 import com.tiklab.matflow.execute.model.MatFlowCode;
@@ -15,21 +13,12 @@ import com.tiklab.matflow.instance.service.execAchieveService.CodeAchieveService
 import com.tiklab.matflow.instance.service.execAchieveService.CommonAchieveService;
 import com.tiklab.matflow.setting.proof.model.Proof;
 import com.tiklab.rpc.annotation.Exporter;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.JGitInternalException;
-import org.eclipse.jgit.transport.*;
-import org.eclipse.jgit.util.FS;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.tmatesoft.svn.core.SVNException;
-import org.tmatesoft.svn.core.SVNURL;
-import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
-import org.tmatesoft.svn.core.wc2.SvnCheckout;
-import org.tmatesoft.svn.core.wc2.SvnOperationFactory;
-import org.tmatesoft.svn.core.wc2.SvnTarget;
+
 
 import java.io.File;
 import java.io.IOException;
@@ -106,14 +95,14 @@ public class CodeAchieveServiceImpl implements CodeAchieveService {
         matFlowExecHistory.setRunLog(matFlowExecHistory.getRunLog()+s);
         matFlowExecHistoryList.add(matFlowExecHistory);
 
-        //开始克隆
-        try {
-        codeStart(proof, matFlowCode,path);
-        } catch (GitAPIException | IOException | SVNException e) {
-            commonAchieveService.updateTime(matFlowProcess,beginTime);
-            commonAchieveService.updateState(matFlowProcess,e.toString(), matFlowExecHistoryList);
-            return 0;
-        }
+        ////开始克隆
+        //try {
+        //codeStart(proof, matFlowCode,path);
+        //} catch (GitAPIException | IOException | SVNException e) {
+        //    commonAchieveService.updateTime(matFlowProcess,beginTime);
+        //    commonAchieveService.updateState(matFlowProcess,e.toString(), matFlowExecHistoryList);
+        //    return 0;
+        //}
 
         //更新状态
         matFlowExecLog.setRunLog( s + "代码拉取成功" + "\n");
@@ -132,34 +121,34 @@ public class CodeAchieveServiceImpl implements CodeAchieveService {
      * @throws GitAPIException git拉取异常
      * @throws IOException 地址找不到
      */
-    public void codeStart(Proof proof, MatFlowCode matFlowCode, String path) throws GitAPIException, IOException, SVNException {
-        if (matFlowCode == null){
-            return;
-        }
-        switch (matFlowCode.getType()){
-            case 1,2,3,4 :
-                if (proof.getProofType().equals("SSH")){
-                    File file = File.createTempFile("matFlow", ".txt");
-                    matFlowCommonService.writePrivateKeyPath(proof.getProofPassword(),file.getAbsolutePath());
-                    sshClone(matFlowCode.getCodeAddress(), file.getAbsolutePath(), path, matFlowCode.getCodeBranch());
-                    file.deleteOnExit();
-                }else {
-                    if (proof.getProofUsername() == null || proof.getProofPassword() == null){
-                        throw new IOException("账户或密码不能为空。");
-                    }
-                    UsernamePasswordCredentialsProvider provider = new UsernamePasswordCredentialsProvider(proof.getProofUsername(), proof.getProofPassword());
-                    clone(path, matFlowCode.getCodeAddress(), provider, matFlowCode.getCodeBranch());
-                }
-                break;
-            case 5:
-                if (proof.getProofType().equals("SSH")){
-                    sshSvn(matFlowCode,proof,path);
-                }else {
-                    svn(matFlowCode,proof,path);
-                }
-                break;
-        }
-    }
+    //public void codeStart(Proof proof, MatFlowCode matFlowCode, String path) throws GitAPIException, IOException, SVNException {
+    //    if (matFlowCode == null){
+    //        return;
+    //    }
+    //    switch (matFlowCode.getType()){
+    //        case 1,2,3,4 :
+    //            if (proof.getProofType().equals("SSH")){
+    //                File file = File.createTempFile("matFlow", ".txt");
+    //                matFlowCommonService.writePrivateKeyPath(proof.getProofPassword(),file.getAbsolutePath());
+    //                sshClone(matFlowCode.getCodeAddress(), file.getAbsolutePath(), path, matFlowCode.getCodeBranch());
+    //                file.deleteOnExit();
+    //            }else {
+    //                if (proof.getProofUsername() == null || proof.getProofPassword() == null){
+    //                    throw new IOException("账户或密码不能为空。");
+    //                }
+    //                UsernamePasswordCredentialsProvider provider = new UsernamePasswordCredentialsProvider(proof.getProofUsername(), proof.getProofPassword());
+    //                clone(path, matFlowCode.getCodeAddress(), provider, matFlowCode.getCodeBranch());
+    //            }
+    //            break;
+    //        case 5:
+    //            if (proof.getProofType().equals("SSH")){
+    //                sshSvn(matFlowCode,proof,path);
+    //            }else {
+    //                svn(matFlowCode,proof,path);
+    //            }
+    //            break;
+    //    }
+    //}
 
     /**
      * git代码拉取
@@ -168,16 +157,16 @@ public class CodeAchieveServiceImpl implements CodeAchieveService {
      * @param credentialsProvider 凭证
      * @param branch 分支
      */
-    private void clone(String gitAddress, String gitUrl, CredentialsProvider credentialsProvider, String branch) throws GitAPIException, JGitInternalException {
-        File fileAddress = new File(gitAddress);
-        Git git = Git.cloneRepository()
-                .setURI(gitUrl)
-                .setCredentialsProvider(credentialsProvider)
-                .setDirectory(fileAddress)
-                .setBranch(branch)
-                .call();
-        git.close();
-    }
+    //private void clone(String gitAddress, String gitUrl, CredentialsProvider credentialsProvider, String branch) throws GitAPIException, JGitInternalException {
+    //    File fileAddress = new File(gitAddress);
+    //    Git git = Git.cloneRepository()
+    //            .setURI(gitUrl)
+    //            .setCredentialsProvider(credentialsProvider)
+    //            .setDirectory(fileAddress)
+    //            .setBranch(branch)
+    //            .call();
+    //    git.close();
+    //}
 
 
     /**
@@ -187,34 +176,34 @@ public class CodeAchieveServiceImpl implements CodeAchieveService {
      * @param clonePath 克隆位置
      * @param branch 分支
      */
-    public void sshClone(String url, String privateKeyPath,String clonePath,String branch) throws GitAPIException, JGitInternalException {
-       
-        Git git;
-        File fileAddress = new File(clonePath);
-        SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
-            @Override
-            protected void configure(OpenSshConfig.Host host, Session session ) {
-                //取消host文件验证
-                session.setConfig("StrictHostKeyChecking","no");
-            }
-            @Override
-            protected JSch createDefaultJSch(FS fs) throws JSchException {
-                JSch defaultJSch = super.createDefaultJSch(fs);
-                defaultJSch.addIdentity(privateKeyPath);
-                return defaultJSch;
-            }
-        };
-        git = Git.cloneRepository()
-                .setURI(url)
-                .setBranch(branch)
-                .setTransportConfigCallback(transport -> {
-                    SshTransport sshTransport = (SshTransport) transport;
-                    sshTransport.setSshSessionFactory(sshSessionFactory);
-                })
-                .setDirectory(fileAddress)
-                .call();
-        git.close();
-    }
+    //public void sshClone(String url, String privateKeyPath,String clonePath,String branch) throws GitAPIException, JGitInternalException {
+    //
+    //    Git git;
+    //    File fileAddress = new File(clonePath);
+    //    SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
+    //        @Override
+    //        protected void configure(OpenSshConfig.Host host, Session session ) {
+    //            //取消host文件验证
+    //            session.setConfig("StrictHostKeyChecking","no");
+    //        }
+    //        @Override
+    //        protected JSch createDefaultJSch(FS fs) throws JSchException {
+    //            JSch defaultJSch = super.createDefaultJSch(fs);
+    //            defaultJSch.addIdentity(privateKeyPath);
+    //            return defaultJSch;
+    //        }
+    //    };
+    //    git = Git.cloneRepository()
+    //            .setURI(url)
+    //            .setBranch(branch)
+    //            .setTransportConfigCallback(transport -> {
+    //                SshTransport sshTransport = (SshTransport) transport;
+    //                sshTransport.setSshSessionFactory(sshSessionFactory);
+    //            })
+    //            .setDirectory(fileAddress)
+    //            .call();
+    //    git.close();
+    //}
 
     /**
      * SVN 拉取代码
@@ -222,19 +211,19 @@ public class CodeAchieveServiceImpl implements CodeAchieveService {
      * @param proof 凭证信息
      * @param path 文件地址
      */
-    public void svn(MatFlowCode matFlowCode, Proof proof, String path) throws SVNException {
-        matFlowCommonService.deleteFile(new File(path));
-        SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
-        char[] password = proof.getProofPassword().toCharArray();
-
-        BasicAuthenticationManager auth = BasicAuthenticationManager.newInstance(proof.getProofUsername(), password);
-        svnOperationFactory.setAuthenticationManager(auth);
-        SvnCheckout checkout = svnOperationFactory.createCheckout();
-        checkout.setSource(SvnTarget.fromURL(SVNURL.parseURIEncoded(matFlowCode.getCodeAddress())));
-        checkout.setSingleTarget(SvnTarget.fromFile(new File(path)));
-        checkout.run();
-        svnOperationFactory.dispose();
-    }
+    //public void svn(MatFlowCode matFlowCode, Proof proof, String path) throws SVNException {
+    //    matFlowCommonService.deleteFile(new File(path));
+    //    SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
+    //    char[] password = proof.getProofPassword().toCharArray();
+    //
+    //    BasicAuthenticationManager auth = BasicAuthenticationManager.newInstance(proof.getProofUsername(), password);
+    //    svnOperationFactory.setAuthenticationManager(auth);
+    //    SvnCheckout checkout = svnOperationFactory.createCheckout();
+    //    checkout.setSource(SvnTarget.fromURL(SVNURL.parseURIEncoded(matFlowCode.getCodeAddress())));
+    //    checkout.setSingleTarget(SvnTarget.fromFile(new File(path)));
+    //    checkout.run();
+    //    svnOperationFactory.dispose();
+    //}
 
     /**
      * SVN SSH拉取代码
@@ -242,18 +231,18 @@ public class CodeAchieveServiceImpl implements CodeAchieveService {
      * @param proof 凭证信息
      * @param path 文件地址
      */
-    public void sshSvn(MatFlowCode matFlowCode, Proof proof, String path) throws SVNException {
-        matFlowCommonService.deleteFile(new File(path));
-        SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
-        BasicAuthenticationManager auth = BasicAuthenticationManager
-                .newInstance(proof.getProofUsername(), new File(proof.getProofPassword()),null,22);
-        svnOperationFactory.setAuthenticationManager(auth);
-        SvnCheckout checkout = svnOperationFactory.createCheckout();
-        checkout.setSource(SvnTarget.fromURL(SVNURL.parseURIEncoded(matFlowCode.getCodeAddress())));
-        checkout.setSingleTarget(SvnTarget.fromFile(new File(path)));
-        checkout.run();
-        svnOperationFactory.dispose();
-    }
+    //public void sshSvn(MatFlowCode matFlowCode, Proof proof, String path) throws SVNException {
+    //    matFlowCommonService.deleteFile(new File(path));
+    //    SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
+    //    BasicAuthenticationManager auth = BasicAuthenticationManager
+    //            .newInstance(proof.getProofUsername(), new File(proof.getProofPassword()),null,22);
+    //    svnOperationFactory.setAuthenticationManager(auth);
+    //    SvnCheckout checkout = svnOperationFactory.createCheckout();
+    //    checkout.setSource(SvnTarget.fromURL(SVNURL.parseURIEncoded(matFlowCode.getCodeAddress())));
+    //    checkout.setSingleTarget(SvnTarget.fromFile(new File(path)));
+    //    checkout.run();
+    //    svnOperationFactory.dispose();
+    //}
 
 
 
