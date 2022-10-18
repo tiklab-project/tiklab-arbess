2329a9130f33197152c689e0752eb1475f040828
zhangcheng <zhq11191750>
2022-10-18 13:21:46 +0800

    配置顺序

--- tiklab-matflow-server/src/main/resources/scripts/pipeline.sql
+++ tiklab-matflow-server/src/main/resources/scripts/pipeline.sql
@@ -1,192 +1,194 @@
-CREATE TABLE `pipeline`  (
-  `pipeline_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '流水线id',
-  `pipeline_name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '流水线名称',
-  `user_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户id',
-  `pipeline_create_time` datetime NULL DEFAULT NULL COMMENT '流水线创建时间',
-  `pipeline_create_type` int NULL DEFAULT NULL COMMENT '流水线类型',
-  `pipeline_collect` int NULL DEFAULT NULL COMMENT '收藏状态',
-  `pipeline_state` int NULL DEFAULT NULL COMMENT '运行状态',
-  `pipeline_power` int NULL DEFAULT NULL COMMENT '项目权限',
-  PRIMARY KEY (`pipeline_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '流水线表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
--- Table build for pipeline_activity
+-- Table structure for pipeline
 -- ----------------------------
-CREATE TABLE `pipeline_activity`  (
-  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `create_time` datetime NULL DEFAULT NULL,
-  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `massage` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `pipeline_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `news` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+create table pipeline  (
+  pipeline_id varchar(256)  COMMENT '流水线id',
+  pipeline_name varchar(256)  COMMENT '流水线名称',
+  user_id varchar(256)  COMMENT '用户id',
+  pipeline_create_time datetime  COMMENT '流水线创建时间',
+  pipeline_create_type int  COMMENT '流水线类型',
+  pipeline_collect int  COMMENT '收藏状态',
+  pipeline_state int  COMMENT '运行状态',
+  pipeline_power int  COMMENT '项目权限',
+  PRIMARY KEY (pipeline_id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_code
+-- Table structure for pipeline_activity
 -- ----------------------------
-CREATE TABLE `pipeline_code`  (
-  `code_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'gitId',
-  `type` int NULL DEFAULT NULL COMMENT '类型',
-  `code_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址名',
-  `code_address` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'git地址',
-  `code_branch` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '分支',
-  `proof_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '凭证id',
-  `sort` int NULL DEFAULT NULL COMMENT '排序',
-  `code_alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '执行名称',
-   `pipeline_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '流水线id',
-  PRIMARY KEY (`code_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+create table pipeline_activity  (
+  id varchar(255) ,
+  create_time datetime ,
+  user_id varchar(255) ,
+  massage varchar(255) ,
+  pipeline_id varchar(255) ,
+  news varchar(255) ,
+  PRIMARY KEY (id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_deploy
+-- Table structure for pipeline_build
 -- ----------------------------
-CREATE TABLE `pipeline_deploy`  (
-  `deploy_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '部署id',
-  `type` int NULL DEFAULT NULL COMMENT '容器类型',
-  `deploy_type` int NULL DEFAULT NULL COMMENT '部署类型',
-  `source_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '打包文件地址',
-  `deploy_address` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '部署文件地址',
-  `start_port` int NULL DEFAULT NULL COMMENT 'docker启动端口',
-  `proof_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '凭证id',
-  `mapping_port` int NULL DEFAULT NULL COMMENT '映射端口',
-  `sort` int NULL DEFAULT NULL COMMENT '排序',
-  `deploy_alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '执行名称',
-  `ssh_ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'IP地址',
-  `ssh_port` int NULL DEFAULT NULL COMMENT '端口号',
-  `start_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '启动文件地址',
-  `deploy_order` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '文件操作',
-  `start_shell` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '启动命令',
-   `pipeline_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '流水线id',
-  PRIMARY KEY (`deploy_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+create table pipeline_build  (
+  build_id varchar(256)  COMMENT '构建id',
+  build_address varchar(256)  COMMENT '文件地址',
+  build_order varchar(256)  COMMENT '构建命令',
+  PRIMARY KEY (build_id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_follow
+-- Table structure for pipeline_code
 -- ----------------------------
-CREATE TABLE `pipeline_follow`  (
-  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `pipeline_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+create table pipeline_code  (
+  code_id varchar(256)  COMMENT 'gitId',
+  code_name varchar(255)  COMMENT '地址名',
+  code_address varchar(256)  COMMENT 'git地址',
+  code_branch varchar(256)  COMMENT '分支',
+  proof_id varchar(256)  COMMENT '凭证id',
+  PRIMARY KEY (code_id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_history
+-- Table structure for pipeline_config_order
 -- ----------------------------
-CREATE TABLE `pipeline_history`  (
-  `history_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'id',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `run_way` int NULL DEFAULT NULL COMMENT '构建方式',
-  `run_status` int NULL DEFAULT NULL COMMENT '执行状态',
-  `run_time` int NULL DEFAULT NULL COMMENT '执行时间',
-  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '执行人',
-  `run_log` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '日志',
-  `pipeline_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '流水线id',
-  `find_number` int NULL DEFAULT NULL COMMENT '构建次数',
-  `find_state` int NULL DEFAULT NULL COMMENT '判断是否正在执行',
-  PRIMARY KEY (`history_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '构建历史表' ROW_FORMAT = DYNAMIC;
+create table pipeline_config_order  (
+  config_id varchar(255) ,
+  create_time datetime ,
+  pipeline_id varchar(255) ,
+  task_id varchar(255) ,
+  task_type int ,
+  task_sort int ,
+  PRIMARY KEY (config_id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_log
+-- Table structure for pipeline_deploy
 -- ----------------------------
-CREATE TABLE `pipeline_log`  (
-  `log_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '日志id',
-  `history_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '历史id',
-  `task_sort` int NULL DEFAULT NULL COMMENT '运行顺序',
-  `task_type` int NULL DEFAULT NULL COMMENT '执行类型',
-  `run_state` int NULL DEFAULT NULL COMMENT '运行状态',
-  `run_time` int NULL DEFAULT NULL COMMENT '运行时间',
-  `run_log` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '运行日志',
-  `task_alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '任务名称',
-  PRIMARY KEY (`log_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+create table pipeline_deploy  (
+  deploy_id varchar(256)  COMMENT '部署id',
+  deploy_type int  COMMENT '部署类型',
+  source_address varchar(255)  COMMENT '打包文件地址',
+  deploy_address varchar(256)  COMMENT '部署文件地址',
+  start_port int  COMMENT 'docker启动端口',
+  proof_id varchar(256)  COMMENT '凭证id',
+  mapping_port int  COMMENT '映射端口',
+  ssh_ip varchar(255)  COMMENT 'IP地址',
+  ssh_port int  COMMENT '端口号',
+  start_address varchar(255)   COMMENT '启动文件地址',
+  deploy_order varchar(255)   COMMENT '文件操作',
+  start_shell longtext  COMMENT '启动命令',
+  PRIMARY KEY (deploy_id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_open
+-- Table structure for pipeline_follow
 -- ----------------------------
-CREATE TABLE `pipeline_open`  (
-  `open_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `pipeline_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `number` int NULL DEFAULT NULL,
-  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`open_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+create table pipeline_follow  (
+  id varchar(255) ,
+  user_id varchar(255) ,
+  pipeline_id varchar(255) ,
+  PRIMARY KEY (id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_proof
+-- Table structure for pipeline_history
 -- ----------------------------
-CREATE TABLE `pipeline_proof`  (
-  `proof_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '凭证id',
-  `proof_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '凭证名',
-  `proof_scope` int NULL DEFAULT NULL COMMENT '作用域',
-  `proof_type` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '凭证类型',
-  `proof_username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '账户',
-  `proof_password` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '密码',
-  `proof_describe` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '凭证描述',
-  `pipeline_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '流水线id',
-  `proof_create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `type` int NULL DEFAULT NULL COMMENT '类型',
-  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户id',
-  PRIMARY KEY (`proof_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '凭证表' ROW_FORMAT = DYNAMIC;
+create table pipeline_history  (
+  history_id varchar(255) COMMENT 'id',
+  create_time datetime COMMENT '创建时间',
+  run_way int  COMMENT '构建方式',
+  run_status int  COMMENT '执行状态',
+  run_time int  COMMENT '执行时间',
+  user_id varchar(255)  COMMENT '执行人',
+  run_log longtext COMMENT '日志',
+  pipeline_id varchar(255)  COMMENT '流水线id',
+  find_number int  COMMENT '构建次数',
+  find_state int  COMMENT '判断是否正在执行',
+  PRIMARY KEY (history_id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_proof_task
+-- Table structure for pipeline_log
 -- ----------------------------
-CREATE TABLE `pipeline_proof_task`  (
-  `task_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `proof_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `pipeline_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`task_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+create table pipeline_log  (
+  log_id varchar(256)  COMMENT '日志id',
+  history_id varchar(256)  COMMENT '历史id',
+  task_sort int  COMMENT '运行顺序',
+  task_type int  COMMENT '执行类型',
+  run_state int  COMMENT '运行状态',
+  run_time int  COMMENT '运行时间',
+  run_log longtext COMMENT '运行日志',
+  PRIMARY KEY (log_id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_build
+-- Table structure for pipeline_open
 -- ----------------------------
-CREATE TABLE `pipeline_build`  (
-  `build_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '构建id',
-  `type` int NULL DEFAULT NULL COMMENT '类型',
-  `build_address` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '文件地址',
-  `build_order` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '构建命令',
-  `sort` int NULL DEFAULT NULL COMMENT '排序',
-  `build_alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '执行名称',
-   `pipeline_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '流水线id',
-  PRIMARY KEY (`build_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+create table pipeline_open  (
+  open_id varchar(255) ,
+  pipeline_id varchar(255) ,
+  number int ,
+  user_id varchar(255) ,
+  PRIMARY KEY (open_id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_test
+-- Table structure for pipeline_path
 -- ----------------------------
-CREATE TABLE `pipeline_test`  (
-  `test_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '测试id',
-  `type` int NULL DEFAULT NULL COMMENT '类型',
-  `test_order` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '测试命令',
-  `sort` int NULL DEFAULT NULL COMMENT '排序',
-  `test_alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '别名',
-   `pipeline_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '流水线id',
-  PRIMARY KEY (`test_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
+create table pipeline_path  (
+  path_id varchar(255) ,
+  path_type int ,
+  path_name varchar(255) ,
+  path_address varchar(255) ,
+  PRIMARY KEY (path_id) USING BTREE
+);
 
 -- ----------------------------
--- Table build for pipeline_path
+-- Table structure for pipeline_proof
 -- ----------------------------
-CREATE TABLE `pipeline_path`  (
-  `path_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `path_type` int NULL DEFAULT NULL,
-  `path_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `path_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`path_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+create table pipeline_proof  (
+  proof_id varchar(256)  COMMENT '凭证id',
+  proof_name varchar(255)  COMMENT '凭证名',
+  proof_scope int  COMMENT '作用域',
+  proof_type varchar(256)  COMMENT '凭证类型',
+  proof_username varchar(256)  COMMENT '账户',
+  proof_password longtext  COMMENT '密码',
+  proof_describe varchar(256)  COMMENT '凭证描述',
+  pipeline_id varchar(255)  COMMENT '流水线id',
+  proof_create_time datetime  COMMENT '创建时间',
+  type int  COMMENT '类型',
+  user_id varchar(255)  COMMENT '用户id',
+  PRIMARY KEY (proof_id) USING BTREE
+);
 
+-- ----------------------------
+-- Table structure for pipeline_proof_task
+-- ----------------------------
+create table pipeline_proof_task  (
+  task_id varchar(255) ,
+  proof_id varchar(255) ,
+  pipeline_id varchar(255) ,
+  PRIMARY KEY (task_id) USING BTREE
+);
 
-CREATE TABLE `pipeline_scm`  (
-  `scm_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `scm_type` int NULL DEFAULT NULL,
-  `scm_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `scm_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`scm_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+-- ----------------------------
+-- Table structure for pipeline_scm
+-- ----------------------------
+create table pipeline_scm  (
+  scm_id varchar(256) ,
+  scm_type int ,
+  scm_name varchar(255) ,
+  scm_address varchar(255) ,
+  PRIMARY KEY (scm_id) USING BTREE
+);
+
+-- ----------------------------
+-- Table structure for pipeline_test
+-- ----------------------------
+create table pipeline_test  (
+  test_id varchar(256)  COMMENT '测试id',
+  test_order varchar(255)  COMMENT '测试命令',
+  PRIMARY KEY (test_id) USING BTREE
+)
+
