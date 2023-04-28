750c1851e937fb4448c02b7abf13c24ce9d8ef3e
zhangcheng <zhangcheng@126.com>
2023-04-28 15:30:00 +0800

    setting文件

--- tiklab-matflow-server/src/main/java/io/tiklab/matflow/task/deploy/service/TaskDeployExecServiceImpl.java
+++ tiklab-matflow-server/src/main/java/io/tiklab/matflow/task/deploy/service/TaskDeployExecServiceImpl.java
@@ -260,11 +260,6 @@ public class TaskDeployExecServiceImpl implements TaskDeployExecService {
 
             }
         };
-        if (process.exitValue() == 0){
-            logger.info(orders + "命令执行完成。。。。");
-            exec.disconnect();
-            return;
-        }
         tasksInstanceService.readCommandExecResult(process, "UTF-8", error(41), taskId);
         exec.disconnect();
     }
