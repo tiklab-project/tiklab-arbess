192fc84b6bfb47056542fd2ec90c78fc9c76cee3
zhangcheng <zhangcheng@126.com>
2023-05-16 10:44:22 +0800

    op_log 1.0.2

--- tiklab-matflow-starter/src/main/java/io/tiklab/matflow/config/MatFlowDsmAutoConfiguration.java
+++ tiklab-matflow-starter/src/main/java/io/tiklab/matflow/config/MatFlowDsmAutoConfiguration.java
@@ -53,6 +53,9 @@ public class MatFlowDsmAutoConfiguration {
                 "user_1.0.1_dml",
                 "oplog_1.0.1_dml"
         });
+        dsmConfig.newVersion("1.0.2", new String[]{
+                "oplog_1.0.2_dml"
+        });
         return dsmConfig;
     }
 
