02c7666f17a46b3e207924fb65a799b33d6c0d01
zhangcheng <zhangcheng@126.com>
2023-05-26 16:53:44 +0800

    认证信息类型变更

---
+++ tiklab-matflow-server/src/main/resources/scripts/pip-pipeline_1.0.2_dml.sql
@@ -0,0 +1,3 @@
+INSERT INTO "pip_auth_server" VALUES ('xpack', 'xpack', 'xpack', '2023-05-19 17:34:47', 1, null, null, NULL, 'http://xpack.tiklab.net', NULL, NULL, 1, '111111', NULL, NULL, NULL, NULL);
+INSERT INTO "pip_auth_server" VALUES ('xcode', 'xcode', 'xcode', '2023-05-19 14:22:22', 1, null, null, NULL, 'http://xcode.tiklab.net', NULL, NULL, 1, '111111', NULL, NULL, NULL, NULL);
+INSERT INTO "pip_auth_server" VALUES ('teston', 'teston', 'teston', '2023-05-22 14:23:21', 1, null, null, NULL, 'http://teston.tiklab.net', NULL, NULL, 1, '111111', NULL, NULL, NULL, NULL);
