1d6533d582ed9b72a16833f533713afc8fe4ddd7
zhangcheng@darthcloud.com <zcamyy@126.com>
2023-06-26 16:48:51 +0800

    更改yaml地址

--- tiklab-matflow-starter/bin/shutdown.sh
+++ tiklab-matflow-starter/bin/shutdown.sh
@@ -15,6 +15,7 @@ fi
 APP_MAIN="io.tiklab.matflow.MatFlowApplication"
 
 PID=0
+
 getPID(){
     javaps=`$JAVA_HOME/bin/jps -l | grep $APP_MAIN`
     if [ -n "$javaps" ]; then
@@ -48,31 +49,6 @@ shutdown(){
         echo "================================================================================================================"
     fi
 }
-
-shutdown
-
-PORT=${1}
-
-stopMysql(){
-  echo "MYSQL PORT ${PORT}"
-  TMP_FILE=mysql.tmp
-  netstat -tlnp | tail -n +3 | awk '{print $4"|"$7}' | grep '[0-9]\+|[0-9]\+' -o >$TMP_FILE
-  for line in `cat $TMP_FILE`; do
-      port=`echo $line | cut -d '|' -f 1`
-      pid=`echo $line | cut -d '|' -f 2`
-      if [ $port -eq ${PORT} ]; then
-          echo "MYSQL PID $pid"
-          kill -9 ${pid}
-      fi
-  done
-  rm -rf $TMP_FILE
-}
-
-if [ "X${PORT}" = "X" ]; then
-  echo "跳过关闭MYSQL"
-else
-  stopMysql
-fi
 
 
 
